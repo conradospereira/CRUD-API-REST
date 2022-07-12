9ddcae34575cfd59188291eb3277f289e7f64aa1
conrado <conrado.spereira@gmail.com>
2022-07-11 21:42:51 -0300

    Create JPA Repository.

---
+++ src/main/java/com/example/mercadoteixeira/inventorycontrol/Controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.example.mercadoteixeira.inventorycontrol.Controller;
+
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//import com.example.mercadoteixeira.inventorycontrol.Repository.ProductRepository;
+
+@RestController
+@RequestMapping({"/product"})
+public class ProductController {
+    
+    //private ProductRepository repository;
+
+    //ProductController(ProductRepository productRepository) {
+    //     this.repository = productRepository;
+    //}
+
+    //@GetMapping
+    //public List findAll() {
+    //    return repository.findAll();
+    //}
+
+
+}
